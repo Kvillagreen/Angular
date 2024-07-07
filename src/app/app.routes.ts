00795b2916466be56a797c0ccac86d3c0ba240d8
unknown <karlmacariolavillaverde@gmail.com>
2024-07-07 19:00:54 +0800

    first commit 7/7/24

---
+++ src/app/app.routes.ts
@@ -0,0 +1,30 @@
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProjectComponent } from './project/project.component';
+import { ErrorComponent } from './error/error.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ResumeComponent } from './resume/resume.component';
+import { AdminComponent } from './admin/admin.component';
+
+export const routes: Routes = [
+    {path:'', redirectTo:"home",pathMatch:'full'},
+    {path:'home', component:HomeComponent},
+    {path:'about', component:AboutComponent},
+    {path:'contact', component:ContactComponent},
+    {path:'project', component:ProjectComponent},
+    {path:'resume', component:ResumeComponent},
+    {path:'project', component:ProjectComponent},
+    {path:'skills', component:SkillsComponent},
+    {path:'admin', component:AdminComponent},
+    {path:'**', component:ErrorComponent},
+    
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports:[RouterModule],
+})
+export class AppRoutingModule{}
